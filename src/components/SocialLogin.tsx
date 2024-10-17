017716f07ff6d39bb45347490de2f68e2e1d73d8
Luís Serralheiro <encavadas@gmail.com>
2024-10-17 18:37:43 +0100

    Improving the login with oauth

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -1,21 +1,27 @@
 import { useApiGet } from "../api/ApiHooks";
 import { Apis } from "../api/Config";
+import apiFor from "../api/Api";
+import { useEffect, useMemo } from "react";
 
 const SocialLogin = () => {
-  const slug = window.location.pathname.match('/auth/login/([^/]+)$')![1];
-  const queryParams = {} as any;
-  window.location.search.substring(1)
-    .split('&')
-    .forEach((s) => {
-      const split = s.split("=");
-      queryParams[split[0]] = split[1]
-    });
-  const { loading } = useApiGet(Apis.Auth.OauthLogin, {}, {
-    id: `${slug}?code=${queryParams.code}&redirect=${encodeURIComponent(window.location.origin + window.location.pathname)}`
-  });
-  if (queryParams.code && !loading) {
-    window.close();
-  }
+
+  useMemo(() => {
+    const slug = window.location.pathname.match('/auth/login/([^/]+)$')![1];
+    const queryParams = {} as any;
+    window.location.search.substring(1)
+      .split('&')
+      .forEach((s) => {
+        const split = s.split("=");
+        queryParams[split[0]] = split[1]
+      });
+    const api = apiFor(Apis.Auth.OauthLogin);
+    api.get({
+      id: `${slug}?code=${queryParams.code}&redirect=${encodeURIComponent(window.location.origin + window.location.pathname)}`
+    }).then(() => {
+      window.close()
+    })
+  }, []);
+
   return <></>;
 };
 
